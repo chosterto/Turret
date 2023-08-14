b56df3ead88fa616573a4465120a94c96a476b5d
John Cho <johnnychox@gmail.com>
2023-08-14 04:29:16 +0100

    improved setup script

--- Turret/setup.sh
+++ Turret/setup.sh
@@ -3,51 +3,81 @@
 #cd ~/
 #mkdir repos && cd repos
 
+set -e
+
+read -p "Install dependencies? (y:n) " install_packages
 read -p "Delete source repos when finished? (y:n) " delete
 
-echo "Installing dependencies"
-echo "-------------------------------------------"
-# GUI and dev tools
-apt install libgtk2.0-dev pkg-config
-# Photo/Video processing
-apt install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
-apt install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-apt install libxvidcore-dev libx264-dev
-# Optimization packages
-apt install libatlas-base-dev gfortran
+if [ $install_packages = "y" ]; then
+	echo "-------------------------------------------"
+	echo "Installing dependencies"
+	echo "-------------------------------------------"
+	# GUI and dev tools
+	sudo apt install libgtk2.0-dev pkg-config
+	# Photo/Video processing
+	sudo apt install libjpeg-dev libtiff5-dev libjasper-dev
+	sudo apt install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+	sudo apt install libxvidcore-dev libx264-dev
+	# Optimization packages
+	sudo apt install libatlas-base-dev gfortran
+fi
 
+echo "-------------------------------------------"
 echo "Fetching repositories"
 echo "-------------------------------------------"
-if [ ! -d WiringPi ]; then
-git clone https://github.com/WiringPi/WiringPi.git
-fi
-if [ ! -d opencv ]; then
-git clone --branch 3.4 https://github.com/opencv/opencv.git
-fi
-if [ ! -d opencv_contrib]; then
-git clone --branch 3.4 https://github.com/opencv/opencv_contrib.git
+WIRING_PI_DIR=$(echo $(find $HOME -type d -name "WiringPi") | cut -d ' ' -f 1)
+OPENCV_DIR=$(echo $(find $HOME -type d -name "opencv") | cut -d ' ' -f 1)
+OPENCV_CONTRIB_DIR=$(echo $(find $HOME -type d -name "opencv_contrib") | cut -d ' ' -f 1)
+
+if [ ! $WIRING_PI_DIR ]; then
+	git clone https://github.com/WiringPi/WiringPi.git
+	WIRING_PI_DIR=$PWD
+else
+	echo "Found WiringPi at $WIRING_PI_DIR"
 fi
 
-mkdir build_cv
-echo "Building WiringPi"
-cd WiringPi/ && ./build
-cd ../build_cv
+if [ ! $OPENCV_DIR ]; then
+	git clone --branch 3.4 https://github.com/opencv/opencv.git
+	OPENCV_DIR=$PWD
+else
+	echo "Found opencv at $OPENCV_DIR"
+fi
+
+if [ ! $OPENCV_CONTRIB_DIR ]; then
+	git clone --branch 3.4 https://github.com/opencv/opencv_contrib.git
+	OPENCV_CONTRIB_DIR=$PWD
+else
+	echo "Found opencv_contrib at $OPENCV_CONTRIB_DIR"
+fi
+
+#echo "Building WiringPi"
+#cd $WIRING_PI_DIR && ./build
+
 echo "Building OpenCV"
+cd $OPENCV_DIR/..
+if [ ! -d build ]; then
+	mkdir build/
+fi
+cd build
 #OpenCL disabled for raspberry pi
-CMAKE_FLAGS = "-D WITH_OPENCL=OFF -D PYTHON3_EXECUTABLE=/usr/bin/python3 -D PYTHON3_INCLUDE_DIR=/usr/include/python3.9 -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules"
+CMAKE_FLAGS="-D WITH_OPENCL=OFF \
+	-D PYTHON3_EXECUTABLE=/usr/bin/python3 \
+	-D PYTHON3_INCLUDE_DIR=/usr/include/python3.9 \
+	-DOPENCV_EXTRA_MODULES_PATH=$OPENCV_CONTRIB_DIR/modules"
 cmake $CMAKE_FLAGS ../opencv
 make -j1
-make install
+sudo make install
 echo "-------------------------------------------"
 echo "BUILDS DONE"
+echo "-------------------------------------------"
 cd ..
 
-if [$delete == "y"]; then
-echo "Removing WiringPi"
-rm -rf WiringPi
-echo "Removing OpenCV"
-rm -rf opencv
-rm -rf opencv_contrib
-rm -rf build_cv
+if [$delete = "y"]; then
+	echo "Removing WiringPi"
+	rm -rf $WIRING_PI_DIR
+	echo "Removing OpenCV"
+	rm -rf $OPENCV_DIR
+	rm -rf $OPENCV_DIR/../build/
+	rm -rf $OPENCV_CONTRIB_DIR
 fi
 
